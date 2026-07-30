fbd7f0a4873d643c6261696619fa9a3d11c82934
Jaider Olaya <olayajaider10@gmail.com>
2026-07-30 17:21:44 -0500

    Se agregaron nuevas cosas al programa verificar orden

--- verificar_orden_lista.py
+++ verificar_orden_lista.py
@@ -3,19 +3,15 @@
 valores = input("\nIngrese los elementos para la lista separados por espacios, por favor: ").strip().lower()
 
 if not valores:
-    print("\nInvalido. La entrada de los elementos no puede estar vacia.")
+    print("\nInvalido. La entrada de los elementos no puede estar vacía.")
     exit()
 
-try:
-    lista = [n for n in valores.split()]
-except ValueError:
-    print("\nError. El elemento ingresado para la lista es invalido")
-    lista = []
-    exit()
-
-if not lista:
-    print("\nInvalido.")
-    exit()
+# try:
+lista = [n for n in valores.split()]
+# except ValueError:
+#     print("\nError. El elemento ingresado para la lista es invalido.")
+#     lista = []
+#     exit()
 
 print(f"\nLista ingresada: {lista}")
 
@@ -26,6 +22,7 @@ for valor in range(len(lista) - 1):
 
 print(f"\nLa lista ingresada esta ordenada: {orden}")
 #Versión con bucle while
+#Función para que el programa pueda continuar por si el usuario lo desea
 def continuar_programa():
     continuar = input("\n¿Desea continuar con el programa?, (s/n): ").strip().lower()
     if continuar in ("s", "si"):
@@ -37,22 +34,21 @@ def continuar_programa():
 while True:
     valores_lista = input("\nIngrese los elementos para la lista, por favor: ").strip().lower()
 
+    #Validación para que la entrada no este vacía
     if not valores_lista:
-        print("\nInvalido. La entrada de los elementos no puede estar vacia.")
+        print("\nInvalido. La entrada de los elementos no puede estar vacía.")
         continue
 
-    try:
-        lista = [n for n in valores_lista.split()]
-    except ValueError:
-        print("\nError. El valor ingresado es invalido. Intentelo de nuevo.")
-        lista = []
-        break
+    lista = [n for n in valores_lista.split()]
 
+    #Verificación por si la lista esta vacía
     if not lista:
+        print("\nInvalido. Lista vacía.")
         continue
 
     print(f"\nLista ingresada: {lista}")
 
+    #Verificador que ayuda a revisar si la lista esta ordenada o no con True o False
     orden = True
     for valor in range(len(lista) - 1):
         if lista[valor] >= lista[valor+1]:
